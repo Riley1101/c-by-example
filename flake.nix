f1acf50b21e9e493b3570e5a0f60cec9a9039f2b
Riley1101 <arkardev00@gmail.com>
2024-02-17 10:14:03 -0500

    added nix flake for development

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+    description = "Develop in C";
+
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+
+    outputs = { self, nixpkgs }: 
+    
+    let
+        system = "x86_64-linux";  
+        pkgs = nixpkgs.legacyPackages.${system};
+    in 
+    {
+        devShells.${system}.default = 
+            pkgs.mkShell 
+            {
+                buildInputs = [ 
+                    pkgs.valgrind
+                    pkgs.gdb
+                    pkgs.pkg-config
+                    pkgs.neofetch
+                    pkgs.gnumake
+                    pkgs.doxygen
+                ];
+                shellHook = ''
+                    echo "Hello, Mom!"
+                '';
+            };
+    };
+}
+
