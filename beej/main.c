75f829db94717184efb3427559abec4f95eda9fa
Riley1101 <arkardev00@gmail.com>
2024-02-21 17:16:01 -0500

    add more libs

--- beej/main.c
+++ beej/main.c
@@ -1,4 +1,9 @@
+#include <assert.h>
+#include <complex.h>
+#include <ctype.h>
+#include <math.h>
 #include <stdalign.h>
+#include <stdarg.h>
 #include <stddef.h>
 #include <stdio.h>
 
@@ -20,6 +25,25 @@ struct s {
 };
 
 int sum(struct s *x) { return x->a + x->b; }
+
+float divide(int a, int b) {
+  assert(b != 0);
+  return (float)a / b;
+}
+
+int add(int count, ...) {
+  int total = 0;
+  va_list args;
+  va_start(args, count);
+  for (int i = 0; i < count; i++) {
+    printf("i is %d\n", i);
+    int n = va_arg(args, int);
+    printf("n is %d\n", n);
+    total += n;
+  }
+  va_end(args);
+  return total;
+}
 
 int main() {
   struct animal a2 = {"spider", 8};
@@ -48,5 +72,16 @@ int main() {
 
   struct s x = {3, 4};
   printf("%d\n", sum(&x));
+  printf("Sum from %d\n", add(3, 1, 2, 3));
+
+  float dives = divide(2, 3);
+  printf("%f\n", dives);
+
+  double complex complexx = 8 + 1.5708 * I;
+  printf("complex %f\n", creal(complexx));
+
+  printf("isalnum %d\n", isalnum('a') ? 1 : 0);
+  printf("isalnum %d\n", isalnum('B') ? 1 : 0);
+  printf("isalnum %d\n", ispunct('?') ? 1 : 0);
   return 0;
 }
